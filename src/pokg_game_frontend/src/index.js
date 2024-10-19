9b925688164005f193357c259aef1ad6b099d263
tkogut <tkogut9@gmail.com>
2024-10-19 15:01:35 +0200

    Initial commit.

---
+++ src/pokg_game_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { pokg_game_backend } from "../../declarations/pokg_game_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await pokg_game_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
